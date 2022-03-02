4bae7c1fe8389528699d7815cb156fb45a64d810
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-03-02 07:56:30 -0800

    added esperiences sub reducer to combine reducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import experiencesReducer from './experiences'
 
-const reducer = combineReducers({ auth, })
+const reducer = combineReducers({ auth,
+experiences: experiencesReducer})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
