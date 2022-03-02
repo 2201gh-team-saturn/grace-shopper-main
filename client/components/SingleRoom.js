e413f93d8ae8d204fd3ed702bcd75934835ed4ab
kroy94 <kelseyroy@mac.com>
2022-03-02 08:35:37 -0500

    beginning work on single room slice including the first pass at the SingleRoom component, adding thunk + action creators to singleRoom store, adding singleroom to combine reducers

---
+++ client/components/SingleRoom.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchRoom, toggleStatus } from '../store/singleRoom'
+
+//there was a thought that I could use this to practice the useContext react hook,
+//and we would pass the context to this like props
+//but for now I'm going to build out a standard Component
+
+
+class Room extends React.Component {
+
+    componentDidMount() {
+        this.props.singleRoom(this.props.match.params.id)
+    }
+
+    addToCart(){
+        //adds something to the cart
+    }
+
+    render(){
+        const { room } = this.props || [];
+        const experiences = room.experiences || [];
+        const experienceList = experiences.map((experience) => {
+            let id = experience.id;
+            return (
+                <div key={id}>
+                    <Link to={`/experiences/${id}`}>
+                        <li>{experience.name}</li>
+                    </Link>
+                </div>
+            )
+        })
+        return (
+            <div id="single-room">
+                <div id="room-info">
+                <img src={room.imageUrl} ALIGN="left"/>
+                <h2>{room.name}</h2>
+                <p>{room.description}</p>
+                <p>Experiences you can only have in this room:
+                    <ul>
+                         {(experiences.length > 0 && experienceList) || (experiences.length === 0 && <li>Check back soon for new experiences</li>)}
+                    </ul>
+                </p>
+                </div>
+                <button type="button" onClick={this.addToCart}>Add to Cart</button>
+            </div>
+        )
+    }
+}
+
+const mapState = (state) => {
+    return {
+        room: state.singleRoom
+    };
+};
+
+const mapDispatch = (dispatch) => {
+    return {
+        singleRoom: (id) => {
+            dispatch(fetchRoom(id))
+        }
+
+    };
+};
+
+export default connect(mapState, mapDispatch)(Robot);
