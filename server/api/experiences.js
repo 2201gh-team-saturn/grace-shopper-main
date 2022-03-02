c3defc77a4141abcbef1cdb0ff5fb98e20ea9252
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-03-02 13:33:07 -0800

    wrote all experiences api route

--- server/api/experiences.js
+++ server/api/experiences.js
@@ -2,6 +2,19 @@ const router = require('express').Router()
 const { Experience } = require('../db')
 const { Room } = require('../db')
 
+// api/experiences
+router.get('/', async (req, res, next) => {
+  try {
+    const experiences = await Experience.findAll();
+    res.json(experiences);
+  } catch (error) {
+    console.error(
+      `so.... I couldn't find any experiences and I think it's your fault check your api route`
+    );
+    next(error);
+  }
+})
+
 router.get('/:id', async (req, res, next) => {
 	try {
 		const experience = await Experience.findByPk(req.params.id, {
--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('/experiences', require('./experiences'))
 router.use('/users', require('./users'))
 router.use('/', require('./rooms'))
 
