4d14904f0d4ca40396be9f9be417981c71f0816f
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-03-03 18:04:38 -0800

    removed price from experience model

--- server/db/models/Experience.js
+++ server/db/models/Experience.js
@@ -1,29 +1,23 @@
 const Sequelize = require('sequelize');
-const db = require('../db')
+const db = require('../db');
 
 module.exports = db.define('experience', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    price: {
-        type: Sequelize.FLOAT,
-        validate: {
-            notEmpty: true
-        }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true,
+    validate: {
+      notEmpty: true,
     },
-    description: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-        validate: {
-            notEmpty: true
-        }
-    },
-    imageUrl: {     
-        type: Sequelize.STRING(1000),
-        defaultValue: ''
-    }
-})
+  },
+  imageUrl: {
+    type: Sequelize.STRING(1000),
+    defaultValue: '',
+  },
+});
