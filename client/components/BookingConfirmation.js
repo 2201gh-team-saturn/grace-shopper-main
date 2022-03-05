d39e56e90ed39f827879bcc524973ae96744e091
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-03-04 22:16:20 -0800

    got user name to render in booking confirmation but in a flex box hole

--- client/components/BookingConfirmation.js
+++ client/components/BookingConfirmation.js
@@ -2,21 +2,34 @@
 
 //we look forward to making your dreams come true
 //travel check list?
+
+<script src="https://kit.fontawesome.com/cccf77e8e9.js" crossorigin="anonymous"></script>
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Footer } from './Footer';
+import { fetchUsers } from '../store/users';
 
 class BookingConfirmation extends Component {
+  componentDidMount() {
+    this.props.loadUsers();
+  }
   render() {
+    const  {username}  = this.props;
     return (
       <div>
-        <h2 className='booking_text'>Congratulations (User) Your Booking's Complete! </h2>
+        <h2 className='booking_text'>Congratulations {username}! Your Booking's Complete! </h2>
+        {/* <h3 className='booking_text'>Here's your packing list:</h3>
+        <ul className='booking_text'>
+          <li> A sunny disposition</li>
+          <li>Your passport </li>
+          <li>A sense of adventure</li>
+          <li>A snack</li>
+         </ul> */}
         <div className='booking_confirmation_container'>
           <img
             className='booking_confirmation_img'
             src='https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2VsZWJyYXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
           />
-          <div>
-          </div>
         </div>
             <h3 className='booking_text'>We look forward to making your dreams come true</h3>
         <Footer />
@@ -25,4 +38,17 @@ class BookingConfirmation extends Component {
   }
 }
 
-export default BookingConfirmation;
+const mapState = (state) => {
+  return {
+    username: state.auth.username,
+    user: state.auth,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapState, mapDispatchToProps)(BookingConfirmation);
