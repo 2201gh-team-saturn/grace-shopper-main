b5d083dd04441e5a792dbd2469ff3b2a0ddf587b
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-03-04 21:47:22 -0800

    played with the CSS some more, happy to change any of it to however we all want, booking page added to routes and rendering

--- client/components/BookingConfirmation.js
+++ client/components/BookingConfirmation.js
@@ -1,43 +1,24 @@
 //thank you for your booking user.name! //how do I get this here?
 
-//hooray ikon, RR ikon?
 //we look forward to making your dreams come true
 //travel check list?
 import React, { Component } from 'react';
 import { Footer } from './Footer';
 
 class BookingConfirmation extends Component {
-  // async componentDidMount() {
-  //   try {
-  //     const { data } = await axios.get('/api/reviews');
-  //     const reviews = data;
-  //     this.setState({reviews});
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
   render() {
     return (
       <div>
-        <h2>Congratulations (User) Your Booking's Complete!'! </h2>
+        <h2 className='booking_text'>Congratulations (User) Your Booking's Complete! </h2>
         <div className='booking_confirmation_container'>
           <img
-            className='homepage_img'
-            src='https://images.unsplash.com/photo-1596394516093-501ba68a0ba6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            className='booking_confirmation_img'
+            src='https://images.unsplash.com/photo-1519834785169-98be25ec3f84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2VsZWJyYXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
           />
-
-          <div className='reviews_container'>
-            <h3> See what others have to say: </h3>
-            {reviews
-              ? reviews.map((review) => (
-                  <p key={review.id}>
-                    "{review.review_text}"<span> - {review.user.username}</span>
-                  </p>
-                ))
-              : ''}
+          <div>
           </div>
         </div>
+            <h3 className='booking_text'>We look forward to making your dreams come true</h3>
         <Footer />
       </div>
     );
