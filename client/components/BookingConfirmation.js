dfb3e18fbe43cc8a0081ebd6bb7069cb656bf8da
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-03-04 20:48:56 -0800

    added add to cart button and started Booking confirmation component

---
+++ client/components/BookingConfirmation.js
@@ -0,0 +1,11 @@
+//thank you for your booking user.name! //how do I get this here?
+
+//hooray ikon, RR ikon?
+//we look forward to making your dreams come true
+//travel check list?
+import React, { Component } from 'react';
+import { Footer } from './Footer';
+
+class BookingConfirmation extends Component {
+
+}
--- client/components/SingleRoom.js
+++ client/components/SingleRoom.js
@@ -45,7 +45,7 @@ class SingleRoom extends React.Component {
                     <div id="single-room-img">
                         <img src={room.imageUrl} />
                     </div>
-                    <div>
+                    <div id='room_info'>
                         <h1>{room.name}</h1>
                         <p>{room.description}</p>
                         <div className="room-info__checks">
@@ -53,7 +53,7 @@ class SingleRoom extends React.Component {
                                 <span className="room-info__check-title">Check in </span>
                                 <span className="room-info__check-time">3 - 5 PM</span>
                             </div>
-                            
+
                             <div className="room-info__check">
                                 <span className="room-info__check-title">Check out </span>
                                 <span className="room-info__check-time">12 - 2 PM</span>
@@ -64,10 +64,11 @@ class SingleRoom extends React.Component {
                             <ul>
                                 {(experiences.length > 0 && experienceList) || (experiences.length === 0 && <li>Check back soon for new experiences</li>)}
                             </ul>
+                            <button type="button" id='add_to_cart_btn' className='button' onClick={this.addToCart}>Add to Cart</button>
                         </div>
                     </div>
                 </div>
-                {/* <button type="button" onClick={this.addToCart}>Add to Cart</button> */}
+
                 <div>
                 </div>
             </div >
