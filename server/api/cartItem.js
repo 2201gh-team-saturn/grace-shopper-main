fd1c593437c4814c9e4646f2b25553aaa90625be
Leah Hamilton <leahhamilton@rodrigos-MacBook-Pro.local>
2022-03-07 14:07:32 -0800

    wrote routes to show all cart items, to post cart items and to delte an item

--- server/api/cartItem.js
+++ server/api/cartItem.js
@@ -28,3 +28,38 @@ router.get('/cartItems', requireToken, async (req, res, next) => {
     next(err);
   }
 });
+
+router.post('/cartItems', requireToken, async (req, res, next) => {
+  try {
+    if (!req.user) {
+      throw new Error('Unauthorized');
+    }
+    const [newCartItem, created] = await CartItem.findOrCreate({
+      where: {
+        id: req.body.id,
+        numberOfNights: req.body.numberOfNights,
+      },
+    });
+    if (created) {
+      res.status(201).send(newCartItem);
+    }
+    res.status(409).send('nope');
+  } catch (error) {
+    console.error('your post cartItem route is broken', error);
+    next(error);
+  }
+});
+
+router.delete('/cartItems', requireToken, async (req, res, next) => {
+  try {
+    if (!req.user) {
+      throw new Error('Unauthorized');
+    }
+    const cartItem = await CartItem.findByPk(req.params.id);
+    cartItem.destroy();
+    res.send(cartItem);
+  } catch (error) {
+    console.error('if you booked that room youre keepin it darnit');
+    next(error);
+  }
+});
