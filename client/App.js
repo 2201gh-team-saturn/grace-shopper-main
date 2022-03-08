03b1027ee38fabbf4359f36e1926aef97c48b26f
kroy94 <kelseyroy@mac.com>
2022-03-07 21:17:18 -0500

    adding the GuestCart component and some react hooks to begin building out localStorage functionality

--- client/App.js
+++ client/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState} from "react";
 
 import Navbar from './components/Navbar'
 import Routes from './Routes'
 
+const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]")
+export const CartContext = React.createContext();
+
 const App = () => {
+  const [cart, setCart] = useState(cartFromLocalStorage);
   return (
     <div>
-      <Navbar />
-      <Routes />
+      <CartContext.Provider
+        value={{
+          cart,
+          // setCartOnLocalStorage: () => {
+          //   setCart(localStorage.setItem("cart", JSON.stringify(cart)))
+          // }
+          setCart,
+          // useEffect
+        }}>
+        <Navbar />
+        <Routes />
+      </CartContext.Provider>
     </div>
   )
 }
